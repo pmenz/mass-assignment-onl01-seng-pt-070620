b40d54f58eabaeff2bef0a511a5f686b93a45f5e
Pablo Menz <p.menz.r@gmail.com>
2020-08-14 06:18:51 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -2,7 +2,9 @@ class Person
   attr_accessor:name, :birthday, :hair_color, :eye_color, :height, 
 :weight, :handed, :complexion, :t_shirt_size, 
 :wrist_size, :glove_size, :pant_length, :pant_width
-def initiliaze()
+def initiliaze(name:, birthday, hair_color, eye_color, height, 
+weight, handed, complexion, t_shirt_size, 
+wrist_size, glove_size, pant_length, pant_width)
 
   #your code here
 end
