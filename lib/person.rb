57c6dc7058fa08ed22c6c0333011f08073dbaf63
Pablo Menz <p.menz.r@gmail.com>
2020-08-14 06:30:50 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -1,25 +1,26 @@
 class Person
   attr_accessor:name, :birthday, :hair_color, :eye_color, :height, 
-:weight, :handed, :complexion, :t_shirt_size, 
-:wrist_size, :glove_size, :pant_length, :pant_width
+  :weight, :handed, :complexion, :t_shirt_size, 
+  :wrist_size, :glove_size, :pant_length, :pant_width
+
+
 def initiliaze(name:, birthday:, hair_color:, eye_color:, height:, 
-weight, handed:, complexion:, t_shirt_size:, 
-wrist_size:, glove_size:, pant_length:, pant_width:)
-
-@name=name
-@birthday=birthday
-@hair_color=hair_color
-@eye_color=eye_color
-@height=height
-@weight=weight
-@handed=handed
-@complexion=complexion
-@t_shirt_size=t_shirt_size
-@wrist_size=wrist_size
-@glove_size=glove_size
-@pant_width=pant_width
-@pant_length=pant_length
-
+  weight, handed:, complexion:, t_shirt_size:, 
+  wrist_size:, glove_size:, pant_length:, pant_width:)
+  @name=name
+  @birthday=birthday
+  @hair_color=hair_color
+  @eye_color=eye_color
+  @height=height
+  @weight=weight
+  @handed=handed
+  @complexion=complexion
+  @t_shirt_size=t_shirt_size
+  @wrist_size=wrist_size
+  @glove_size=glove_size
+  @pant_width=pant_width
+  @pant_length=pant_length
+end
 
   #your code here
 end
